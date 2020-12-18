Fixed computation of R0

generate_figures.py
@@ -263,7 +263,7 @@ def sensitivity_analysis(name,values,symbol,load_performances=True):
     for value in values:
         performances_plot(name="concentrated",
                           init_file=init_path("concentrated.csv"),
-                          file_name="concentrated_{}_{}".format(symbol,value),
+                          file_name="concentrated_{}_{}".format(symbol,value).replace('.','_'),
                           shown_starts=[
                               'Adaptive mobility regions res=800000',
                               'Adaptive mobility regions res=30000',
@@ -295,19 +295,10 @@ def sensitivity_analysis_v(v_min=3 ,v_max=7.5, symbol="v",load_performances=True
     # figure takes long to generate for resolution = 20 (45 minutes), test with resolution=4 (<1 min)
     # resolution = 20 means 21*21 pixels
 def effective_reproduction_number(resolution=20):
-    from numpy import linalg as LA
     from .rivm_loader import rivm
-    from .mobility import mobility
 
     # No infections initialized.
     init_df = rivm.concentrated_init(0, "Eindhoven")
-    areas = init_df.index
-
-    infectious_period = CoronaConstants.infectious_period
-    contacts_average = CoronaConstants.contacts_average
-    transmission_prob = CoronaConstants.transmission_prob
-    population_nl = CoronaConstants.population_nl
-    average_total_mobility = CoronaConstants.average_total_mobility
 
     grid = []
     for p in range(1,resolution+2):
@@ -315,23 +306,8 @@ def effective_reproduction_number(resolution=20):
         for alpha in range (1,resolution+2):
             fraction_local_contacts=(p-1)/(resolution)
             fraction_tested=(alpha-1)/(resolution)
-            contacts_per_visit = (1-fraction_local_contacts)*contacts_average*population_nl/(
-                2*average_total_mobility)
-
-            # two factors which go in fron on the diagonal and off-diagonal spots, respectively:
-            local_R = transmission_prob*contacts_average*fraction_local_contacts*infectious_period
-            mobile_R = (1-fraction_tested)*infectious_period*contacts_per_visit*transmission_prob
-
-            r_matrix = np.array([
-                [
-                    local_R if a1==a2 else mobile_R * mobility[a1,a2] / susceptibles
-                    for a2 in areas
-                ]
-                for a1, susceptibles in init_df['susceptible'].items()
-            ])
-            # Find largest eigenvalue
-            w,_ = LA.eig(r_matrix)
-            row.append(max(abs(w)))
+            seir = MobilitySEIR(init_df,constants=CoronaConstants(fraction_local_contacts=fraction_local_contacts,fraction_tested=fraction_tested))
+            row.append(seir.basic_reproduction_number())
         grid.append(row)
     
     # Plot numpy array:

mobility_seir.py
@@ -58,7 +58,7 @@ class MobilitySEIR:
         self.contacts_local = constants.contacts_local
         self.infectious_period = constants.infectious_period
         self.latent_period = constants.latent_period
-        self.alpha = constants.fraction_tested
+        self.fraction_tested = constants.fraction_tested
 
     # Region should either be a label in self.division or a subset of the areas.
     def simulate_region(self, region):
@@ -100,9 +100,9 @@ class MobilitySEIR:
                         It + Int
                 ) / N
                 # Exposed to infected_tested
-                infections_tested = E * self.alpha / self.latent_period
+                infections_tested = E * self.fraction_tested / self.latent_period
                 # Exposed to infected_nottested
-                infections_nottested = E * (1 - self.alpha) / self.latent_period
+                infections_nottested = E * (1 - self.fraction_tested) / self.latent_period
                 # infected_tested to removed_tested
                 removals_tested = It / self.infectious_period
                 # infected_nottested to removed_nottested
@@ -248,6 +248,18 @@ class MobilitySEIR:
         import numpy as np
         return 1 - np.exp(-self.initial_divergence())
 
+    def basic_reproduction_number(self):
+        from numpy import linalg as LA
+        K = next_generation_matrix(
+            population=sum(self.init_df[c] for c in self.compartments),
+            const=self.constants,
+            mob=self.mobility,
+            areas=self.init_df.index
+        )
+        w, v = LA.eig(K.transpose())
+        basic_reproduction_number = max(abs(w))
+        return basic_reproduction_number
+
 
 def load_initialization(init_name):
     return pd.read_csv(init_path(init_name), index_col='name')
@@ -272,3 +284,35 @@ def generate_initializations():
     evenlydistributed.to_csv(init_path('evenlydistributed.csv'))
     rivm0310.to_csv(init_path("historical0310.csv"))
     rivm0421.to_csv(init_path("historical0421.csv"))
+
+def next_generation_matrix(population=None, susceptible=None, mob=None, const=CoronaConstants, areas=None):
+    import numpy as np
+    import itertools as it
+    if mob is None:
+        from .mobility import mobility
+        mob = mobility
+    if population is None:
+        from .mezuro_preprocessing import gemeente_shapes
+        population = gemeente_shapes['inhabitant']
+    if susceptible is None:
+        susceptible = population
+    if areas is None:
+        areas = list(mob.G.nodes)
+    # Diagonal entries in Next Generation Matrix are:
+    local_basic_reproduction_number = (
+            const.contacts_local * const.transmission_prob * const.infectious_period
+    )
+    # Off-diagonal entries depend on mobility times a constant factor
+    off_diagonal_factor = (
+            (1 - const.fraction_tested) * const.contacts_per_visit * const.transmission_prob * const.infectious_period
+    )
+    # Next Generation Matrix:
+    K = np.zeros((len(areas),len(areas)))
+    for (i,area),(j,other_area) in it.product(enumerate(areas),enumerate(areas)):
+        if i == j:
+            K[i, j] = local_basic_reproduction_number * susceptible[area] / population[area]
+        else:
+            K[i, j] = off_diagonal_factor * (
+                (mob[area,other_area] + mob[other_area,area]) / population[area]
+            ) * susceptible[other_area] / population[other_area]
+    return K